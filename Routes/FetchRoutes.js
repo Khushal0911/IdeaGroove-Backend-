11d2e4c46465f8a6e0f192fb795b99e86fd0b83c
Khushal Bafna <khushalbafna0911@gmail.com>
2026-01-25 15:08:10 +0530

    lol

---
+++ Routes/FetchRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getData,
+  searchColleges,
+  searchDegrees,
+  searchHobbies,
+} from "../controllers/FetchController.js";
+const fetchRouter = express.Router();
+
+fetchRouter.get("/search", getData);
+fetchRouter.get("/search/colleges", searchColleges);
+fetchRouter.get("/search/degrees", searchDegrees);
+fetchRouter.get("/search/hobbies", searchHobbies);
+
+export default fetchRouter;
