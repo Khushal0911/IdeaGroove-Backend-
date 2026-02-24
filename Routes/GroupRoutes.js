7311380c622ba7ddd56ef380617b726a8ce26131
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2026-02-25 01:01:05 +0530

    Completed Groups page

--- Routes/GroupRoutes.js
+++ Routes/GroupRoutes.js
@@ -1,14 +1,23 @@
 import express from "express";
-import { addGroup, deleteGroup, getGroups, getUserGroups, joinGroup, leaveGroup, updateGroup, viewMembers } from "../controllers/GroupController.js";
+import {
+  addGroup,
+  deleteGroup,
+  getGroups,
+  getUserGroups,
+  joinGroup,
+  leaveGroup,
+  updateGroup,
+  viewMembers,
+} from "../controllers/GroupController.js";
 
 const groupRouter = express.Router();
 
-groupRouter.get("/",getGroups);
+groupRouter.get("/", getGroups);
 groupRouter.get("/user/:id", getUserGroups);
-groupRouter.post("/create",addGroup);//working
-groupRouter.post("/joinGroup",joinGroup); //working
-groupRouter.post("/leaveGroup",leaveGroup); //working
-groupRouter.post("/update",updateGroup);//working
-groupRouter.get("/delete/:Room_ID",deleteGroup);//working
-groupRouter.get("/viewMembers/:Room_ID",viewMembers);
+groupRouter.post("/create", addGroup); //working
+groupRouter.post("/joinGroup", joinGroup); //working
+groupRouter.post("/leaveGroup", leaveGroup); //working
+groupRouter.post("/update", updateGroup); //working
+groupRouter.get("/delete/:Room_ID", deleteGroup); //working
+groupRouter.get("/viewMembers/:Room_ID", viewMembers);
 export default groupRouter;
