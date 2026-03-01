74b81bb0896f6f8037f323113c6f3c51385857b7
Khushal Bafna <khushalbafna0911@gmail.com>
2026-03-01 17:24:39 +0530

    Admin Panel changes

--- Routes/ComplaintRoutes.js
+++ Routes/ComplaintRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { addComplaint, deleteComplaint, getAllComplaints, getUserComplaints } from "../controllers/ComplaintController.js";
+import {
+  addComplaint,
+  deleteComplaint,
+  getAllComplaints,
+  getUserComplaints,
+} from "../controllers/ComplaintController.js";
+import { updateComplaintStatus } from "../Controllers/AdminController.js";
 
 const complaintRouter = express.Router();
 
-complaintRouter.get("/",getAllComplaints);
-complaintRouter.get("/user/:id",getUserComplaints);
-complaintRouter.post("/create",addComplaint);
-complaintRouter.delete("/delete/:id",deleteComplaint);
+complaintRouter.get("/", getAllComplaints);
+complaintRouter.get("/user/:id", getUserComplaints);
+complaintRouter.post("/create", addComplaint);
+complaintRouter.delete("/delete/:id", deleteComplaint);
+complaintRouter.post("/update-status", updateComplaintStatus);
 
 export default complaintRouter;
