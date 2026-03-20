9a56e8a581e5a641e54c345bef75f2ce0a31ad43
Rudra Prajapati <prajapatirudra817@gmail.com>
2026-03-20 23:14:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: true,
+  ssl: {
+  rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
